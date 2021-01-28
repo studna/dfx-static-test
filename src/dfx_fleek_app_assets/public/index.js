4e96780d3e63b0b347ee4c1af13ec754cb757db8
studna <vojtech.studenka@gmail.com>
2021-01-28 17:15:01 +0100

    Initial commit

--- src/dfx_fleek_app_assets/public/index.js
+++ src/dfx_fleek_app_assets/public/index.js
@@ -1,47 +1,65 @@
 import dfx_fleek_app_assets from "ic:canisters/dfx_fleek_app_assets";
 
-const getText = bytes => (
-  new TextDecoder().decode(new Uint8Array(bytes))
-)
+const getText = (bytes) => new TextDecoder().decode(new Uint8Array(bytes));
 
 const injectScript = (jsBytes) => {
-  const scriptNode = document.createElement('script');
+  const scriptNode = document.createElement("script");
   scriptNode.innerHTML = getText(jsBytes);
-  document.body.appendChild(scriptNode)
-}
+  document.body.appendChild(scriptNode);
+};
 
 const injectStyle = (jsBytes) => {
-  const styleNode = document.createElement('style');
+  const styleNode = document.createElement("style");
   styleNode.innerHTML = getText(jsBytes);
-  document.body.appendChild(styleNode)
-}
+  document.body.appendChild(styleNode);
+};
 
 const getInjectingFunc = (fileName) => {
   if (/\.css$/.test(fileName)) {
-    return injectStyle
+    return injectStyle;
   }
   if (/\.js$/.test(fileName)) {
-    return injectScript
+    return injectScript;
   }
-  return (bytes) => console.error('This type of resource is not handled yet: ', fileName, getText(bytes));
-}
-
-const injectResources = jsonBytes => {
-  const json = JSON.parse(getText(jsonBytes));
-  const scriptsToLoad = Object.values(json).flatMap(value => Object.values(value).flatMap(fileNamesList => fileNamesList))
-
-  console.log('scripts to load: ', scriptsToLoad)
-  scriptsToLoad.forEach(fileName => {
-    dfx_fleek_app_assets.retrieve(fileName).then(getInjectingFunc(fileName))
-  })
+  return (bytes) =>
+    console.error(
+      "This type of resource is not handled yet: ",
+      fileName,
+      getText(bytes)
+    );
 };
 
+// const injectResources = jsonBytes => {
+//   const json = JSON.parse(getText(jsonBytes));
+//   const scriptsToLoad = Object.values(json).flatMap(value => Object.values(value).flatMap(fileNamesList => fileNamesList))
+
+//   console.log('scripts to load: ', scriptsToLoad)
+//   scriptsToLoad.forEach(fileName => {
+//     dfx_fleek_app_assets.retrieve(fileName).then(getInjectingFunc(fileName))
+//   })
+// };
+
+const injectResources = (jsonBytes) => {
+  const json = JSON.parse(getText(jsonBytes));
+
+  const entrypoints = json.entrypoints;
+
+  Object.values(json.files).forEach((filename) =>
+    entrypoints.push(filename.substr(1))
+  );
+
+  console.log("scripts to load: ", entrypoints);
+  entrypoints.forEach((fileName) => {
+    console.log("retrieve", fileName);
+    dfx_fleek_app_assets.retrieve(fileName).then(getInjectingFunc(fileName));
+  });
+};
 
 const initInjectingResources = async () => {
-  console.log('initInjectingResources');
-  const result = await dfx_fleek_app_assets.retrieve('build-manifest.json')
-  console.log('const result = await dfx_fleek_app_assets.retrieve()');
-  injectResources(result)
-}
-console.log('bootstrap...');
+  console.log("initInjectingResources");
+  const result = await dfx_fleek_app_assets.retrieve("asset-manifest.json");
+  console.log("const result = await dfx_fleek_app_assets.retrieve()");
+  injectResources(result);
+};
+console.log("bootstrap...");
 initInjectingResources();
